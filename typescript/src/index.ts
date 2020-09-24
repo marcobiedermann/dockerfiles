c73914dd1f2d937e1bb577c5b05f7d20ac32a553
Marco Biedermann <marco.biedermann@gmx.de>
2020-09-24 16:28:58 +0200

    feat: TypeScript
    Add Docker TypeScript setup

---
+++ typescript/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const config = {
+  port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+};
+
+const app = express();
+
+app.get("/", (_request, response) => response.send("Hello, world!"));
+
+app.listen(config.port, () =>
+  console.log(`App is running at http://localhost:${config.port}.`)
+);
