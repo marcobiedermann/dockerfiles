792d1f8e21806040243a6b7cd5faba9151cea356
Marco Biedermann <marco.biedermann@gmx.de>
2020-09-26 19:00:01 +0200

    feat: PostgreSQL
    Add Docker PostgreSQL DB setup

---
+++ node-postgres/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { Client } = require("pg");
+
+const config = {
+  port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+};
+
+const app = express();
+
+const client = new Client(
+  "postgres://postgres:example@postgres:5432/postgres"
+);
+
+client.connect()
+  .then(() => console.log('connected'))
+  .catch(error => console.error('connection error', error.stack))
+
+app.get("/", (_request, response) => response.send("Hello, world!"));
+
+app.listen(config.port, () =>
+  console.log(`App is running at http://localhost:${config.port}.`)
+);
