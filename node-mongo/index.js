b3d5b51ae21a4d0e6e30cc781737798b3aaf072d
Marco Biedermann <marco.biedermann@gmx.de>
2020-09-26 09:12:41 +0200

    feat: Mongo
    Add Docker Mongo DB setup

---
+++ node-mongo/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const config = {
+  port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+};
+
+mongoose
+  .connect("mongodb://mongo:27017/node-mongo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to Database"))
+  .catch((error) => console.error(error));
+
+const app = express();
+
+app.get("/", (_request, response) => response.send("Hello, world!"));
+
+app.listen(config.port, () =>
+  console.log(`App is running at http://localhost:${config.port}.`)
+);
